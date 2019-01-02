f611f020266bdbfda749865d4949cea5e2593b66
laucy <1170664318@qq.com>
2019-01-02 23:32:02 +0800

    添加了models里边剩下的表

---
+++ Hospital/Models/AssessmentItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Odbc;
+
+namespace Hospital.Models
+{
+    public class AssessmentItem
+    {
+        public int IT_ID { get; set; }
+        public string IT_Name { get; set; }
+        public int I_ID { get; set; }
+        public float IT_Price { get; set; }
+        
+
+        public AssessmentItem() { }
+
+        public AssessmentItem(int itid, string itname, int iid,float itprice)
+        {
+            IT_ID = itid;
+            IT_Name = itname;
+            I_ID = iid;
+            IT_Price = itprice;
+        }
+
+        public static List<AssessmentItem> getList(OdbcDataReader reader)
+        {
+            List<AssessmentItem> list = new List<AssessmentItem>();
+            AssessmentItem assessments;
+            while (reader.Read())
+            {
+                assessments = new AssessmentItem();
+                assessments.IT_ID = reader.GetInt32(0);
+                assessments.IT_Name = reader.GetString(1);
+                assessments.I_ID = reader.GetInt32(2);
+                assessments.IT_Price = reader.GetFloat(3); 
+                list.Add(assessments);
+            }
+            return list;
+        }
+    }
+}
---
+++ Hospital/Models/Hospitalization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Odbc;
+
+namespace Hospital.Models
+{
+    public class Hospitalization
+    {
+
+        public int C_ID { get; set; }
+        public int S_ID { get; set; }
+        public DateTime H_In { get; set; }
+        public DateTime H_Out { get; set; }
+        public float H_Sum { get; set; }
+
+        public Hospitalization() { }
+
+        public Hospitalization(int cid, int sid,DateTime hin, DateTime hout,
+             float hsum)
+        {
+            C_ID = cid;
+            S_ID = sid;
+            H_In = hin;
+            H_Out =hout;
+            H_Sum = hsum;          
+        }
+
+        public static List<Hospitalization> getList(OdbcDataReader reader)
+        {
+            List<Hospitalization> list = new List<Hospitalization>();
+            Hospitalization hospitalizations;
+            while (reader.Read())
+            {
+                hospitalizations = new Hospitalization();
+                hospitalizations.C_ID = reader.GetInt32(0);
+                hospitalizations.S_ID = reader.GetInt32(1);
+                hospitalizations.H_In = reader.GetDate(2);
+                hospitalizations.H_Out = reader.GetDate(3);
+                hospitalizations.H_Sum = reader.GetFloat(4);               
+                list.Add(hospitalizations);
+            }
+            return list;
+        }
+    }
+}
