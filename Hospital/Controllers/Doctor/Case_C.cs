1600817d92354ddb4c96d4f6dbc55c60aeb07a33
laucy <1170664318@qq.com>
2019-01-03 13:47:36 +0800

    添加test_c.cs

--- Hospital/Controllers/Doctor/Case_C.cs
+++ Hospital/Controllers/Doctor/Case_C.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Hospital.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
 using System.Linq;
 using System.Web;
 
@@ -14,6 +17,24 @@ namespace Hospital.Controllers.Doctor
                 "values('" + Convert.ToInt32(pid) + "', '" + Convert.ToInt32(eid) + "'" +
                 ",'" + ccomplain + "','" + cdiagnose + "','" + cadvice + "')";
             return Tool.ExecuteSQL.ExecuteNonQuerySQL_GetBool(sql);
+        }
+        //根据病人id查询病历id
+        public static List<Case> SelectCID(string patientid)
+        {
+            string sql = "SELECT * FROM  `case` WHERE `P_ID`='"+patientid+"'";
+            OdbcConnection odbcConnection =DB. DBManager.GetOdbcConnection();
+            odbcConnection.Open();
+            OdbcCommand odbcCommand = new OdbcCommand(sql, odbcConnection);
+            OdbcDataReader odbcDataReader = odbcCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            if (odbcDataReader.HasRows)
+            {
+                List<Case> list = Case.getList(odbcDataReader);
+                odbcConnection.Close();
+                return list;
+            }
+            else
+                odbcConnection.Close();
+            return null;
         }
     }
 }
---
+++ Hospital/Controllers/Test/Test_C.cs
@@ -0,0 +1,35 @@
+﻿using Hospital.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hospital.Controllers
+{
+    public class Test_C
+    {
+        //已知病人id查询检查项目和价格
+        public static List<Test> SelectFuzzy(string info)
+        {
+            string sql = "SELECT * FROM employee "
+                + "WHERE `E_ID` LIKE '%" + info + "%'"
+                + "OR `E_Name` LIKE '%" + info + "%'"
+                + "OR `E_Sex` LIKE '%" + info + "%'"
+                + "OR `E_Phone` LIKE '%" + info + "%'"
+                + "OR `E_Position` LIKE '%" + info + "%'";
+            OdbcConnection odbcConnection = DBManager.GetOdbcConnection();
+            odbcConnection.Open();
+            OdbcCommand odbcCommand = new OdbcCommand(sql, odbcConnection);
+            OdbcDataReader odbcDataReader = odbcCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            if (odbcDataReader.HasRows)
+            {
+                List<Employee> list = Employee.getList(odbcDataReader);
+                odbcConnection.Close();
+                return list;
+            }
+            else
+                odbcConnection.Close();
+            return null;
+        }
+    }
+}
